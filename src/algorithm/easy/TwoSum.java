f877fa1ed60a2a93d58445e0cad23926c3d496c7
KyleCe <kcstep@126.com>
2017-04-30 23:22:43 +0800

    [TwoSum] solution

---
+++ src/algorithm/easy/TwoSum.java
@@ -0,0 +1,19 @@
+package algorithm.easy;
+
+/**
+ * Created by Kyle on 30/04/2017.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int c = nums[left] + nums[right];
+            if (c == target) return new int[]{left + 1, right + 1};
+            else if (c > target) right--;
+            else left++;
+        }
+        return null;
+    }
+}
