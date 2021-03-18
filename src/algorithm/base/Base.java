8c67f5ea71fdd9082d68e8a58ba15690d354c754
玄竹 <xuanzhu.cg@alibaba-inc.com>
2021-03-19 07:43:39 +0800

    [add] ImageSmoother.java--s2

--- src/algorithm/base/Base.java
+++ src/algorithm/base/Base.java
@@ -13,6 +13,22 @@ public class Base {
 
     }
 
+    public boolean equal(int[][] A, int[][] B) {
+        return equal(A, B, true);
+    }
+
+    public boolean equal(int[][] A, int[][] B, boolean printLog) {
+        for (int i = 0, j = 0, lenR = A.length, lenC = A[0].length; i < lenR && j < lenC; i++, j++) {
+            if (A[i][j] != B[i][j]) {
+                if (printLog) {
+                    log("not equal:", "i=", i, ", j=", j, ", A[i][j]=", A[i][j], ", B[i][j]=", B[i][j]);
+                }
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void log(Object... os) {
         if (os == null || os.length == 0) {
             System.out.println();
--- src/algorithm/easy/ImageSmoother.java
+++ src/algorithm/easy/ImageSmoother.java
@@ -30,14 +30,18 @@ public class ImageSmoother extends Base {
     public void test() {
         {
             int[][] input = {
-                    {1, 1, 1},
-                    {1, 0, 1},
-                    {1, 1, 1},
+                    {2, 3, 4},
+                    {5, 6, 7},
+                    {8, 9, 10},
+                    {11, 12, 13},
+                    {14, 15, 16},
             };
             int[][] output = {
-                    {0, 0, 0},
-                    {0, 0, 0},
-                    {0, 0, 0},
+                    {4, 4, 5},
+                    {5, 6, 6},
+                    {8, 9, 9},
+                    {11, 12, 12},
+                    {13, 13, 14},
             };
             assertTrue(equal(imageSmoother(input), output));
         }
@@ -48,27 +52,95 @@ public class ImageSmoother extends Base {
         assertEquals(1f, Math.floor(1.0), 0.0);
     }
 
-    private boolean equal(int[][] A, int[][] B) {
-        for (int i = 0, j = 0, len1 = A.length, len2 = A[0].length; i < len1 && j < len2; i++, j++) {
-            if (A[i][j] != B[i][j]) return false;
-        }
-        return true;
-    }
-
     public int[][] imageSmoother(int[][] M) {
         int lenR = M.length;
         int lenC = M[0].length;
         int[][] result = new int[lenR][lenC];
+        //int[][] RB = new int[lenR][lenC];
+        //int[] calResult;
         for (int i = 0; i < lenR; i++) {
             for (int j = 0; j < lenC; j++) {
-                result[i][j] = cal(M, i, j);
+                //if (i - 1 >= 0 && j - 1 >= 0 && RB[i - 1][j - 1] != 0) {
+                //    calResult = calWithLT(M, i, j, RB[i - 1][j - 1]);
+                //} else {
+                //    calResult = calFully(M, i, j);
+                //}
+                //result[i][j] = calResult[0];
+                //RB[i][j] = calResult[1];
+
+                result[i][j] = calOrigin(M, i, j);
             }
         }
 
         return result;
     }
 
-    private int cal(int[][] M, int i, int j) {
+    private int[] calWithLT(int[][] M, int i, int j, int lt) {
+        int iStart = Math.max(i - 1, 0);
+        int jStart = Math.max(j - 1, 0);
+        int iEnd = Math.min(i + 1, M.length - 1);
+        int jEnd = Math.min(j + 1, M[0].length - 1);
+
+        int rb = 0, cur;
+        int sum = lt;
+        //log("calWithLT:i=", i, ",j=", j, ",lt=", lt);
+
+        for (int m = iStart; m <= iEnd; m++) {
+            for (int n = jStart; n <= jEnd; n++) {
+                cur = M[m][n];
+
+                if (m <= i && n <= j) {
+                    // do nothing
+
+                } else {
+                    sum += cur;
+                    ////log("sum:m=", m, ",n=", n, ",cur=", cur, ",sum=", sum);
+                }
+
+                if (m >= i && n >= j) {
+                    ////log("RB:m=", m, ",n=", n);
+                    rb += cur;
+                }
+
+                //count++;
+            }
+        }
+
+        int count = ((iEnd - iStart + 1) * (jEnd - jStart + 1));
+
+        //log("calWithLT result:i=", i, ",j=", j, ",lt=", lt, ",re=", sum / count);
+        return new int[]{sum / count, rb};
+    }
+
+    private int[] calFully(int[][] M, int i, int j) {
+        int iStart = Math.max(i - 1, 0);
+        int jStart = Math.max(j - 1, 0);
+        int iEnd = Math.min(i + 1, M.length - 1);
+        int jEnd = Math.min(j + 1, M[0].length - 1);
+
+        int rb = 0;
+        int sum = 0;
+        ////log("calFully:i=", i, ",j=", j);
+        int rbOrigin = 0;
+
+        for (int m = iStart; m <= iEnd; m++) {
+            for (int n = jStart; n <= jEnd; n++) {
+                sum += M[m][n];
+                if (m >= i && n >= j) {
+                    rbOrigin = rb;
+                    //log("calFully RB:m=", m,",n=",n,"M[m][n]=",M[m][n], ", rbOrigin=", rbOrigin, ", rb=", rb);
+                    rb += M[m][n];
+                }
+                //count++;
+            }
+        }
+
+        //log("calFully result:i=", i, ",j=", j, ",re=", sum / count);
+        int count = ((iEnd - iStart + 1) * (jEnd - jStart + 1));
+        return new int[]{sum / count, rb};
+    }
+
+    private int calOrigin(int[][] M, int i, int j) {
         int iStart = Math.max(i - 1, 0);
         int jStart = Math.max(j - 1, 0);
         int iEnd = Math.min(i + 1, M.length - 1);
